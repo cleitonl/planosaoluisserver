114d9327d55259f5cc13669fc501b2dd50ed1314
CLeiton luiz <cleitoncostaeng@gmail.com>
2022-04-01 10:03:33 -0300

    criação de modo dev

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const { useNewUrlParser, useUnifiedTopology, useCreateIndex, userName, pass, dataBase } = require('./config/configs.json');
 
-  const uri = `mongodb+srv://${userName}:${pass}@cluster0.lhoc3.mongodb.net/${dataBase}?retryWrites=true&w=majority`;
+const uriProd = `mongodb+srv://${userName}:${pass}@cluster0.lhoc3.mongodb.net/${dataBase}?retryWrites=true&w=majority`;
+const uriDev = `mongodb://localhost:27017/${dataBase}`;
 const connect = async () => {
   try {
-    await mongoose.connect(uri, { useNewUrlParser, useUnifiedTopology, useCreateIndex,  useFindAndModify: false });
+    await mongoose.connect(uriDev, { useNewUrlParser, useUnifiedTopology, useCreateIndex, useFindAndModify: false });
     console.log('Db connected!')
   } catch (error) {
     handleError(error);
